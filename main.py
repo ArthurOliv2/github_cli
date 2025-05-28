2e9e408a32986cebdbca8829cb5dc83710690c68
ArthurOliv2 <arthuroliveirap2014@gmail.com>
2025-05-28 14:03:49 -0300

    Adiciona função para buscar atividade do github via API

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import sys
+import urllib.request
+import json
+
+def fetch_github_CLI(username):
+    url = f"https://api.github.com/users/{username}/events"
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = json.loads(response.read().decode())
+            return data
+    except urllib.error.HTTPError as e:
+        print(f"Erro ao buscar dados: {e.reason}")
+        return None
 
 if __name__ == "__main__":
     print("Bem-vindo à Github_CLI! Use: github-cli <username>")
